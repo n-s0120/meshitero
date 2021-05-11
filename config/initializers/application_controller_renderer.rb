dc10310682f91cb6d19dc8caf9d7419aa5518c09
n-s0120 <satoshicuni@gmail.com>
2021-05-11 07:23:56 +0000

    meshitero-0511-1

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '7a7c4acb106ac4fa7486ab3093d5fe96f652c382934b3c2b53a73e4c3ca54529b8596fceffde15613375c7bb12cd7a844a7eda099207b9d13ed126b13b546ec1'
